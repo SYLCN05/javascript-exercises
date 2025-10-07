95969e5124df4e1f6ca73e92d75e2fd7977eab53
SYLCN05 <siyaryalcin25@gmail.com>
2025-10-07 18:39:37 +0200

    solved javascript exercises
    all exercises make usage of
    the test cases, so that the desired output gets tested

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num <0){
+        return "ERROR"
+    }
+    else{
+         let word = ""
+    for(let i = 1; i<2;i++){
+        for(let j = 0; j <num; j++){
+          word += string
+        }
+    }
+    return word
+    }
+   
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total = 0;
+if(start > 0 && end > 0 && Number.isInteger(start) && Number.isInteger(end) ){
+    if(start > end){
+        for(let i = start; i>=end; i--){
+            total += i
+            
+        }
+    }
+    else if(start<end){
+        for(let i = start; i<=end; i++){
+            total += start
+            start++
+        }
+    }
+   
+    return total
+ }
+ return "ERROR"
 };
 
 // Do not edit below this line
